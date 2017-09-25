0fa028f7e3183a95da873461438e055fa3ad3fa2
Eslam Hamouda <eslamx7@gmail.com>
2017-09-25 22:39:39 +0200

    refactor filtering the webhook payload to be with LINQ

--- SparkPostWebhooksSample/Controllers/WebhooksController.cs
+++ SparkPostWebhooksSample/Controllers/WebhooksController.cs
@@ -22,31 +22,23 @@ namespace SparkPostWebhooksSample.Controllers
         public async Task<IActionResult> ReceiveEventsAsync([FromBody] JArray payload)
         {
             var unsubscribe_events = new string[] { "bounce", "list_unsubscribe", "spam_complaint", "out_of_band", "link_unsubscribe" };
-            var extracted_emails = new HashSet<string>();
 
             if (!ModelState.IsValid)
                 return BadRequest();
 
-            foreach (var obj in payload)
-            {
-                var msys = obj["msys"] as JObject;
-                var event_type = msys?.Properties().FirstOrDefault();
-                if (event_type != null)
-                {
-                    var message_paylod = msys[event_type.Name] as JObject;
-                    var message_type = message_paylod.Property("type")?.Value.ToString();
-                    if (unsubscribe_events.Contains(message_type))
-                    {
-                        var recipient_address = message_paylod.Property("rcpt_to")?.Value.ToString();
-                        extracted_emails.Add(recipient_address);
-                    }
-                }
-            }
+            var extracted_emails = payload.OfType<JObject>()
+                                          .Select(obj => obj["msys"]).Cast<JObject>()
+                                          .Where(msys => msys.Properties().FirstOrDefault() != null)
+                                          .Select(msys => msys.Properties().FirstOrDefault().Value).Cast<JObject>()
+                                          .Where(evt => unsubscribe_events.Contains(evt["type"].ToString()))
+                                          .Select(msg => msg["rcpt_to"].ToString())
+                                          .Distinct()
+                                          .ToList();
 
             foreach (var email in extracted_emails)
             {
-                var subscriber = _db.Subscribers.FirstOrDefault(s=>s.Email == email);
-                if(subscriber != null)
+                var subscriber = _db.Subscribers.FirstOrDefault(s => s.Email == email);
+                if (subscriber != null)
                 {
                     subscriber.Subscribed = false;
                 }
